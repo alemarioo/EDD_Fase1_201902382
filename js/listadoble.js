83904b1ddbfcc264ecb5d4353730a5b6eed86566
mario ruano <marioruano25@gmail.com>
2021-12-16 20:17:33 -0600

    feature: metodo para agregar elementos lista doble

---
+++ js/listadoble.js
@@ -0,0 +1,39 @@
+class ListaDobleEnlazda{
+    constructor(){
+        this.inicio = null;
+        this.final = null;
+    }
+
+    add(mes){
+        if (this.vacio()) {
+            this.inicio = this.final = new NodoDoble(null, null, mes);
+        }else{
+            const nuevo = new NodoDoble(this.final, null, mes);
+            this.final.siguiente = nuevo;
+            this.final = nuevo;
+        }
+    }
+
+    vacio(){
+        return this.inicio === null;
+    }
+}
+
+class NodoDoble{
+    constructor(anterio, siguiente, mes){
+        this.anterior = anterio;
+        this.siguiente = siguiente;
+        this.calendario = null;
+        this.mes = mes;
+    }
+}
+
+console.log("a");
+
+const x = new ListaDobleEnlazda();
+x.add(1);
+x.add(2);
+x.add(3);
+x.add(4);
+x.add(5);
+x.add(6);
