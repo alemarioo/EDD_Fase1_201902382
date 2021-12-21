129be3e0365b0e96d7ca0fde2f6801d767efde6a
mario ruano <marioruano25@gmail.com>
2021-12-21 11:09:44 -0600

    fix: matriz dinamica

--- js/MatrizDinamica.js
+++ js/MatrizDinamica.js
@@ -1,15 +1,61 @@
 class MatrizDinamica{
     constructor() {
         this.inicio = new NodoMatriz( 0, null, null, null, null)
+        this.DiaAux = null
+        this.HoraAux = null
     }
 
     agregar(dato, dia, hora){
-        const fila = this.getDia(dia);
-        const hora = this.getHora(hora);
+        this.getDia(dia);
+        this.getHora(hora);
+
+        let aux = this.HoraAux;
+        
+        while (aux) {
+            let actual = aux.derecho
+            if (!actual) {
+                actual = new NododATO(hora, dia, null, aux, null, null);
+                actual.izquie.derecho = actual
+                aux = actual
+                break;
+            }
+
+            if (actual.dia > dia ) {
+                const nuevo = new NododATO(hora, dia, null, this.HoraAux, aux, null)
+                actual.izquie.derecho = nuevo
+                actual.izquie = nuevo
+                aux = nuevo
+                break;
+            }
+            aux = aux.derecho
+        }
+        
+        let axuFila = this.DiaAux
+        
+        while (axuFila) {
+            let actual = axuFila.abajo
+            if (!actual) {
+                axuFila.abajo = aux
+                aux.arriba = axuFila
+                break;
+            }
+            if (axuFila.hora > hora ) {
+                aux.arriba = axuFila.arriba 
+                aux.abajo = axuFila
+
+                axuFila.arriba.abajo = aux
+                axuFila.arriba = aux
+                break
+            }
+            axuFila = axuFila.abajo
+        }
+
+
     }
     /**
      * Funcion que retorna la fila del dia
      * @param {number} dia 
+     * @returns {NodoMatriz} nodo nuevo o que encuentra en al matriz
      */
     getDia(dia){
         let aux = this.inicio;
@@ -19,6 +65,7 @@ class MatrizDinamica{
             aux = new NodoMatriz(dia, null, aux, null, aux?.derecho);
             aux.izquie = this.inicio;
             aux.izquie.derecho = aux;
+            this.DiaAux = aux
             return aux;
         }
         
@@ -32,6 +79,7 @@ class MatrizDinamica{
                     anterior.derecho.izquie = nuevo;
                 }    
                 anterior.derecho = nuevo;
+                this.DiaAux = nuevo
                 return nuevo;
             }
 
@@ -44,12 +92,17 @@ class MatrizDinamica{
                 if (nuevo.derecho) {
                     anterior.derecho.izquie = nuevo;
                 }    
+                this.DiaAux = nuevo
                 return nuevo;
             }
             
         };
     }
-
+    /**
+     * 
+     * @param {number} hora 
+     * @returns {NodoMatriz} nodo nuevo o que encuentra en al matriz
+     */
     getHora(hora){
         let aux = this.inicio;
 
@@ -58,6 +111,7 @@ class MatrizDinamica{
             aux = new NodoMatriz(hora, aux, null, aux?.abajo, null);
             aux.arriba = this.inicio;
             aux.arriba.abajo = aux;
+            this.HoraAux = aux
             return aux
         }
         
@@ -71,6 +125,7 @@ class MatrizDinamica{
                     anterior.abajo.arriba = nuevo;
                 }    
                 anterior.abajo = nuevo;
+                this.HoraAux = nuevo
                 return nuevo;
             }
 
@@ -83,6 +138,7 @@ class MatrizDinamica{
                 if (nuevo.abajo) {
                     anterior.abajo.arriba = nuevo;
                 }    
+                this.HoraAux = nuevo
                 return nuevo;
             }
             
@@ -90,7 +146,26 @@ class MatrizDinamica{
     }
 
     toString(){
-
+        let aux = this.inicio;
+        let auxcolum = aux.derecho
+        let cadena = "";
+        while(aux){
+            
+            while (auxcolum){
+                
+                if (auxcolum.dato) {
+                    cadena += auxcolum.dato + " ->";    
+                }else{
+                    cadena += auxcolum.dia + "," + auxcolum.hora + " ->";    
+                }
+                
+                auxcolum = auxcolum.derecho
+            }
+            cadena += "\n";
+            aux = aux.abajo;
+            auxcolum = aux?.derecho;
+        }
+        return cadena
     }
 }
 
@@ -100,10 +175,10 @@ class NodoMatriz{
     /**
      * 
      * @param {} dato 
-     * @param {NodoMatriz} arriba 
-     * @param {NodoMatriz} izquierdo 
-     * @param {NodoMatriz} Abajo 
-     * @param {NodoMatriz} derecho 
+     * @param {NododATO} arriba 
+     * @param {NododATO} izquierdo 
+     * @param {NododATO} Abajo 
+     * @param {NododATO} derecho 
      */
     constructor(dato, arriba, izquierdo, Abajo, derecho) {
         this.izquie = izquierdo;
@@ -113,10 +188,26 @@ class NodoMatriz{
         this.dato = dato
     }
 }
-const x = new MatrizDinamica();
-x.agregar(0,2,1)
-x.agregar(0,3,2)
-x.agregar(0,4,3)
-x.agregar(0,7,4)
-x.agregar(0,6,5)
-x.agregar(0,1,6)
+
+
+class NododATO{
+    
+    /**
+     * 
+     * @param {number} hora
+     * @param {number} dia
+     * @param {NodoMatriz} arriba 
+     * @param {NodoMatriz} izquierdo 
+     * @param {NodoMatriz} Abajo 
+     * @param {NodoMatriz} derecho 
+     */
+    constructor(hora, dia, arriba, izquierdo, Abajo, derecho) {
+        this.izquie = izquierdo;
+        this.abajo = Abajo;
+        this.derecho = derecho;
+        this.arriba = arriba;
+        this.hora = hora
+        this.dia = dia
+    }
+}
+
